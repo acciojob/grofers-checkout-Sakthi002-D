03e7b27acc450537b46b80809b979934be8c2dfa
Sakthi002-D <sakthivel.d002@gmail.com>
2026-05-23 06:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const price = document.querySeletorAll(".price");
+
+	let total = 0;
+	price,forEach((price) {
+		total += parseFloat(price.tectContent);
+	});
+
+	const newCell = document.createElement("tr");
+	const newRow = document.createElement("td");
+
+	newCell.textContent = total;
+	newCell.colSpan = 2;
+
+	newRow.appendChild(newCell);
+	document.querySelector("table").appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
